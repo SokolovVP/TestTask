8da3e2226b9753c1e524f8c41ea72a7ca1d8392d
SokolovVP <Sokolovvp02@mail.ru>
2024-07-11 00:00:08 +0400

    Added DI extension method for Currency library

---
+++ Currency/DependencyInjection.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace CurrencyApplication;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddCurrencyServices(this IServiceCollection services)
+    {
+        return services
+            .AddScoped<IExchangeRateProvider, ExchangeRateJsonProvider>()
+            .AddScoped<IExchangeRateRepository, ExchangeRateRepository>()
+            .AddSingleton<IMoneyConverter, MoneyConverter>()
+            .AddSingleton<IMoneyOperations, MoneyOperations>();
+    }
+}
--- TestTask/Program.cs
+++ TestTask/Program.cs
@@ -1,57 +1,14 @@
-﻿//using Microsoft.Extensions.DependencyInjection;
-//using CurrencyApplication;
+﻿using CurrencyApplication;
+using Microsoft.Extensions.DependencyInjection;
 
-//var ServiceCollection = new ServiceCollection().AddScoped<IExchangeRateProvider, ExchangeRateJsonProvider>();
-//var ServiceProvider = ServiceCollection.BuildServiceProvider();
-//IExchangeRateProvider? _rateProvider = ServiceProvider.GetService<IExchangeRateProvider>();
+var Services = new ServiceCollection().AddCurrencyServices();
+var ServiceProvider = Services.BuildServiceProvider();
 
-//var cur = new CurrencyRepository(_rateProvider);
+var _moneyConverter = ServiceProvider.GetService<IMoneyConverter>();
 
-
-//MoneyConverter moneyConverter = new MoneyConverter(cur);
-//MoneyOperations moneyOperations = new MoneyOperations(moneyConverter);
-
-//Money GBPSum = new Money(CurrencyList.GBP, 4);
-//Money RubSum = moneyConverter.ConvertToNewCurrency(GBPSum, CurrencyList.RUB);
-//Console.WriteLine($"SUM IN GBP: {GBPSum.Amount} = {RubSum.Amount} {(CurrencyList)RubSum.CurrentCurrency}");
+Money money1 = new Money(CurrencyList.EUR, 5);
+Money? moneyRub = _moneyConverter?.ConvertToNewCurrency(money1, CurrencyList.RUB);
 
 
-//Money CNYSum = new Money(CurrencyList.CNY, 5);
-//RubSum = moneyConverter.ConvertToNewCurrency(CNYSum, CurrencyList.RUB);
-//Console.WriteLine($"SUM IN CNY: {CNYSum.Amount} = {RubSum.Amount} {(CurrencyList)RubSum.CurrentCurrency}");
+Console.WriteLine($"5 eur = {moneyRub?.Amount} rub");
 
-
-//Money CHFSum = new Money(CurrencyList.CHF, 6);
-//RubSum = moneyConverter.ConvertToNewCurrency(CHFSum, CurrencyList.RUB);
-//Console.WriteLine($"SUM IN CHF: {CHFSum.Amount} = {RubSum.Amount} {(CurrencyList)RubSum.CurrentCurrency}");
-
-
-//Money AEDSum = new Money(CurrencyList.AED, 8);
-//RubSum = moneyConverter.ConvertToNewCurrency(AEDSum, CurrencyList.RUB);
-//Console.WriteLine($"SUM IN AED: {AEDSum.Amount} = {RubSum.Amount} {(CurrencyList)RubSum.CurrentCurrency}");
-
-
-//Console.WriteLine($"{CNYSum.Amount} {(CurrencyList)CNYSum.CurrentCurrency} + " +
-//    $"{CHFSum.Amount} {(CurrencyList)CHFSum.CurrentCurrency} = " +
-//    $"{moneyOperations.Add(CNYSum, CHFSum, CurrencyList.RUB).Amount} ");
-
-//Console.WriteLine($"{GBPSum.Amount} {(CurrencyList)GBPSum.CurrentCurrency} - " +
-//    $"{AEDSum.Amount} {(CurrencyList)AEDSum.CurrentCurrency} = " +
-//    $"{moneyOperations.Sub(GBPSum, AEDSum, CurrencyList.RUB).Amount} ");
-
-
-
-
-
-
-
-using CurrencyApplication;
-
-IExchangeRateProvider rateProvider = new ExchangeRateJsonProvider();
-
-var Rates = await rateProvider.GetExchangeRatesAsync();
-
-foreach (var rate in Rates)
-{
-    Console.WriteLine($"{rate.CurrentCurrency} - {rate.TargetCurrency} = {rate.ExchangeRateValue}");
-}
