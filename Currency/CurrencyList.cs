e274df03c36907f5e74aaf94d9f07ce4af0e1595
SokolovVP <Sokolovvp02@mail.ru>
2024-07-10 14:21:52 +0400

    Implemented CurrencyList and ExchangeRate

--- Currency/CurrencyList.cs
+++ Currency/CurrencyList.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace CurrencyApplication;
 
-namespace Currency
+public enum CurrencyList
 {
-    internal class CurrencyList
-    {
-    }
+    EUR, RUB, USD, RSD, JPY, GBP, CNY, CHF, AED, MXN
 }
